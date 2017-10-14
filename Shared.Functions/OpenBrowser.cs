6c9dec3f3724c3545593112a043bc1767001d4be
Fernando Camacho Garza <fernando.camacho@definityfirst.com>
2017-10-14 15:59:29 -0500

    Fix configuration for firefox

--- Shared.Functions/OpenBrowser.cs
+++ Shared.Functions/OpenBrowser.cs
@@ -12,24 +12,23 @@ namespace Shared.Functions
             get; set;
         }
 
-        //public /*string ProfileDirectoryName*/{ get; set; }
-
-        public OpenBrowser(string url, string browser = "Firefox", string profileDirectoryName = null)
+        public OpenBrowser(string url, string browser = "Firefox", string driverDirectoryLocation = null)
         {
             browser = browser.ToString().ToUpper();
             switch (browser)
             {
                 case "FIREFOX":
-                    FirefoxProfile firefoxProfile = string.IsNullOrEmpty(profileDirectoryName)
-                        ? new FirefoxProfile() : new FirefoxProfile(profileDirectoryName);
-                    firefoxProfile.AcceptUntrustedCertificates = true;
-                    firefoxProfile.SetPreference("dom.max_script_run_time", 0);
-                    Driver = new FirefoxDriver(firefoxProfile);
+                    var service = FirefoxDriverService.CreateDefaultService(driverDirectoryLocation);
+                    var options = new FirefoxOptions();
+                    options.Profile = new FirefoxProfile();
+                    options.Profile.AcceptUntrustedCertificates = true;
+                    options.Profile.SetPreference("dom.max_script_run_time", 0);
+                    Driver = new FirefoxDriver(service, options, System.TimeSpan.FromSeconds(20));
                     Driver.Navigate().GoToUrl(url);
                     break;
                 case "CHROME":
-                    Driver = string.IsNullOrEmpty(profileDirectoryName)
-                        ? new ChromeDriver() : new ChromeDriver(profileDirectoryName);
+                    Driver = string.IsNullOrEmpty(driverDirectoryLocation)
+                        ? new ChromeDriver() : new ChromeDriver(driverDirectoryLocation);
                     Driver.Manage().Window.Maximize();
                     Driver.Manage().Cookies.DeleteAllCookies();
                     Driver.Navigate().GoToUrl(url);
@@ -39,15 +38,13 @@ namespace Shared.Functions
                     IECapabilities.InitialBrowserUrl = url;
                     IECapabilities.IntroduceInstabilityByIgnoringProtectedModeSettings = true;
                     IECapabilities.IgnoreZoomLevel = true;
-                    Driver = string.IsNullOrEmpty(profileDirectoryName)
+                    Driver = string.IsNullOrEmpty(driverDirectoryLocation)
                         ? new InternetExplorerDriver(IECapabilities)
-                        : new InternetExplorerDriver(profileDirectoryName, IECapabilities);
+                        : new InternetExplorerDriver(driverDirectoryLocation, IECapabilities);
                     Driver.Manage().Cookies.DeleteAllCookies();
                     Driver.Manage().Window.Maximize();
                     break;
             }
-            //Driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(5));
         }
-
     }
 }
